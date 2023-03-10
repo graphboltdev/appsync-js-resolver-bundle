a276949592ced8367764fc41aaad3d5ca0f459c4
bboure <benoit.boure@gmail.com>
2023-03-10 17:31:21 +0100

    improvement

--- build.sh
+++
@@ -1,3 +0,0 @@
-esbuild resolvers/createPost.js --bundle --outdir=build --external:"@aws-appsync/utils" --format=esm
-esbuild resolvers/createAuthor.js --bundle --outdir=build --external:"@aws-appsync/utils" --format=esm
-esbuild resolvers/updateAuthor.js --bundle --outdir=build --external:"@aws-appsync/utils" --format=esm
